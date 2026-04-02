890582fdf885ae6e3b8b1794dcd61b8eaa4c567b
robantblast <robanthony850@gmail.com>
2026-04-02 10:30:27 +0100

    added playrightt for aws

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingIncludes: {
+    "/api/send-campaign": ["./node_modules/@sparticuz/chromium/bin/**"]
+  }
 };
 
 module.exports = nextConfig;
